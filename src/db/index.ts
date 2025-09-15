32f211949683c442bc44525811c92caebb17292d
Karan <karansethia24@gmail.com>
2025-09-16 01:22:14 +0530

    Update: DB creation and schema migration
    Changelog
    - Drizzle ORM setup
    - DB connection and development db setup

---
+++ src/db/index.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/node-postgres';
+
+import { config } from "dotenv"
+
+config({ path: ".env.local" })
+
+export const db = drizzle(process.env.DATABASE_URL!, { logger: true });
+
